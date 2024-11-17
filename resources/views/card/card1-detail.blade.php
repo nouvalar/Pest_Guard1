087d93909e9609cbf06d945571447c41434a5cf7
nouvalar <nouvalamr12@gmail.com>
2024-11-18 01:02:36 +0700

    finishing

--- resources/views/card/card1-detail.blade.php
+++ resources/views/card/card1-detail.blade.php
@@ -1,12 +1,21 @@
 @extends('layout.main')
 
+@section('sidebar')
+    @include('partials.sidebar')
+@endsection
+
 @section('content')
     <div class="bg-blur"></div>
     <div class="container mt-5 position-relative">
         <div class="card">
             <div class="card-body">
                 <h1 class="card-title">Card Title 1</h1>
-                <p class="card-text">This is the detailed content for Card 1.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. 
+                    Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. 
+                    Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. 
+                    Egestas integer eget aliquet nibh praesent. 
+                    In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac..</p>
                 <a href="/petunjuk" class="btn btn-primary">Back to Cards</a>
             </div>
         </div>
@@ -23,6 +32,8 @@
             z-index: 1;
             /* Pastikan di atas background */
             position: relative;
+            margin-left: 260px; /* Tambahkan jarak dari sidebar */
+            padding: 20px; /* Opsional: jarak dalam */
         }
     </style>
 @endsection
